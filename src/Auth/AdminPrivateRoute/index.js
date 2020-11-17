70030518f1a6ddb0f2f79bcb87611a03c9154aeb
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-11-17 18:30:58 +0200

    seeting private router for admins

---
+++ src/Auth/AdminPrivateRoute/index.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import SecondDashboard from '../../components/Dashboard/SecondDashboard';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const currentUser = useContext(AuthContext).currentUser;
+    return (
+        <Route
+            {...rest}
+            render={
+                props => {
+                    // return currentUser.email ==='test@test.com' ? <Component {...props} /> : <Redirect to='/login' />
+                    if (currentUser) {
+                        return currentUser.email === 'admin@admin.com' ? <Component {...props} /> : <SecondDashboard {...props} />
+                    } else {
+                        return <Redirect to='/admin' />
+                    }
+                }
+            }
+        >
+        </Route>
+    );
+};
+
+export default PrivateRoute;
