a0c76b2aed3f86477a8ad70702d90da6f9a345b8
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-11-23 22:53:21 +0200

    changing the way currentUser is saved

--- src/Auth/AdminPrivateRoute/index.js
+++ src/Auth/AdminPrivateRoute/index.js
@@ -5,12 +5,12 @@ import axios from 'axios';
 import SecondDashboard from '../../components/Dashboards/SecondaryAdminsDashboard/SecondaryAdminsDashboard';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    const currentUser = useContext(AuthContext).currentUser;
+    const currentUser = localStorage.getItem('currentUser');
     const [isMainAdmin, setIsMainAdmin] = useState(false);
 
     useEffect(() => {
         if (currentUser) {
-            axios.get(`https://e-election-e4023.firebaseio.com/admins/${(currentUser.email).split(".")[0]}/auth.json`)
+            axios.get(`https://e-election-e4023.firebaseio.com/admins/${(currentUser).split(".")[0]}/auth.json`)
                 .then(res => {
                     setIsMainAdmin(res.data);
                 }).catch(err => console.log(err));
@@ -24,7 +24,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
             render={
                 props => {
                     // change it later sabbagh
-                    if (true) {
+                    if (currentUser) {
                         return isMainAdmin ?
                             <Component {...props} /> : <SecondDashboard {...props} />
                     } else {
