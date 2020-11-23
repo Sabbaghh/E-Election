6514805a270b59d14e6e55e5b9448b2d9bdc9a4f
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-11-23 22:54:44 +0200

    adding adminAddPanel

---
+++ src/components/UI/inputs/AddAdminForm/AddAdminForm.js
@@ -0,0 +1,84 @@
+import React, { useState, useContext, useEffect } from 'react';
+import Spinner from '../../Spinners/Spinner';
+import { AuthContext } from '../../../../Auth/context/AuthContext';
+import './AddAdminForm.css'
+
+const AddAdminForm = () => {
+    const [toggle, setToggle] = useState(false);
+    const [show, setShow] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const signup = useContext(AuthContext).signup;
+    const currentUser = useContext(AuthContext).currentUser;
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let emailValue = email;
+        let passwordValue = password;
+        try {
+            setError('');
+            setLoading(true);
+            await signup(emailValue, passwordValue);
+        } catch {
+            setError('failed');
+        }
+        setLoading(false);
+    }
+    const onCancel = () => {
+        setEmail('');
+        setPassword('');
+        setToggle(false);
+    }
+    useEffect(() => {
+        console.log(currentUser);
+    })
+
+    return (
+        <div className='AddAdminForm'>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                {loading && <Spinner />}
+                <div className='Add-admin-email'>
+                    <div className='icon'>
+                        <i className="fas fa-users-cog"></i>
+                    </div>
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="text"
+                        value={email}
+                        disabled={!toggle} />
+                    <div
+                        className='custom-button btn'
+                        onClick={() => setToggle(!toggle)}>
+                        <i className="fas fa-edit"></i></div>
+                </div>
+
+                {
+                    toggle &&
+                    <div className='password-gen'>
+                        <div className='icon'>
+                            <i className="fas fa-key"></i>
+                        </div>
+                        <input
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            type={show ? 'text' : 'password'}
+                            value={password} />
+                        <div className='custom-button btn'
+                            onClick={() => setShow(!show)}>
+                            <i className="fas fa-eye-slash"></i>
+                        </div>
+                    </div>
+                }
+                <div className='submitAndCancel'>
+                    <button className='SubmitButton'> SAVE</button>
+                    <div onClick={() => onCancel()} className='CancelButton'> CANCEL</div>
+                </div>
+                {error && <h1 style={{ margin: '10px', color: '#d62828' }}>{error}</h1>}
+
+            </form>
+        </div>
+
+    );
+};
+
+export default AddAdminForm;
