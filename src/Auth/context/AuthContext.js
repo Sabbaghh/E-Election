4a48e7b736ba5bc62b86c0605b6605e9035aff4b
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-11-27 01:29:29 +0200

    removing unused functions

--- src/Auth/context/AuthContext.js
+++ src/Auth/context/AuthContext.js
@@ -1,40 +1,30 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { auth } from '../../FireBase/fireBase';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState();
-    // const [loading, setLoading] = useState(false);
-
     const signup = (email, PWD) => {
         return auth.createUserWithEmailAndPassword(email, PWD);
+
     }
     const login = (email, PWD) => {
-        auth.onAuthStateChanged(user => {
-            localStorage.setItem('currentUser', user.email);
-        })
-        return auth.signInWithEmailAndPassword(email, PWD);
+        return auth.signInWithEmailAndPassword(email, PWD)
     }
     const resetPassword = (email) => {
         return auth.sendPasswordResetEmail(email);
     }
     const logout = () => {
-        localStorage.setItem('currentUser', '');
         return auth.signOut();
     }
 
-    // useEffect(() => {
-    //     console.log(chnageUser);
-    //     const unSub = auth.onAuthStateChanged(user => {
-    //         if (chnageUser) {
-    //             setCurrentUser(user);
-    //             localStorage.setItem('currentUser', user.email);
-    //         }
-    //         setLoading(false);
-    //     });
-    //     return unSub;
-    // }, [chnageUser]);
+    useEffect(() => {
+        const unSub = auth.onAuthStateChanged((user) => {
+            setCurrentUser(user);
+        });
+        return unSub;
+    }, []);
 
 
     const value = {
