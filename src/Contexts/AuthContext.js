aa2a17c06c15c0c18a2c09c229d506bd439a2596
Sabbaghh <afsabbagh9@gmail.com>
2021-04-01 10:25:06 +0300

    Finshed main admin phase

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -1,44 +1,37 @@
-import React, { createContext, useState, useEffect } from 'react';
-import { auth } from '../FireBase/fireBase';
+import React, { createContext, useState, useEffect } from 'react'
+import { auth } from '../FireBase/fireBase'
 
-export const AuthContext = createContext();
+export const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState();
-    const signup = (email, PWD) => {
-        return auth.createUserWithEmailAndPassword(email, PWD);
+	const [currentUser, setCurrentUser] = useState()
+	const signup = (email, PWD) => {
+		return auth.createUserWithEmailAndPassword(email, PWD)
+	}
+	const login = (email, PWD) => {
+		return auth.signInWithEmailAndPassword(email, PWD)
+	}
+	const resetPassword = (email) => {
+		return auth.sendPasswordResetEmail(email)
+	}
+	const logout = () => {
+		return auth.signOut()
+	}
 
-    }
-    const login = (email, PWD) => {
-        return auth.signInWithEmailAndPassword(email, PWD)
-    }
-    const resetPassword = (email) => {
-        return auth.sendPasswordResetEmail(email);
-    }
-    const logout = () => {
-        return auth.signOut();
-    }
+	useEffect(() => {
+		const unSub = auth.onAuthStateChanged((user) => {
+			setCurrentUser(user)
+		})
+		return unSub
+	}, [])
 
-    useEffect(() => {
-        const unSub = auth.onAuthStateChanged((user) => {
-            setCurrentUser(user);
-        });
-        return unSub;
-    }, []);
-
-
-    const value = {
-        currentUser,
-        setCurrentUser,
-        signup,
-        login,
-        resetPassword,
-        logout
-    }
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    );
-};
-
+	const value = {
+		currentUser,
+		setCurrentUser,
+		signup,
+		login,
+		resetPassword,
+		logout,
+	}
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
--- src/MainAdminDashboard/MainAdminDashBoard.js
+++ src/MainAdminDashboard/MainAdminDashBoard.js
@@ -1,65 +1,70 @@
-import React, { useState, createContext } from 'react';
-import InnerNav from '../Shared/InnerNav';
-import AdminsNavBar from '../Shared/AdminsNavBar';
-import logo from '../assests/logo/e-election-logo.png';
-import LogoElement from '../Shared/logoElement';
-import ProfileSetting from '../Shared/ProfileSetting';
-import WelcomeMsg from '../Shared/WelcomeMsg';
-import SearchInput from '../Shared/SearchInput';
-import Colleges from './Colleges';
-import CollegeInformations from '../Shared/CollegeInformationsContainer';
-import CollegeItems from '../Shared/CollegeItems';
-import DefaultPage from './DefualtPage';
+import React, { useState, createContext } from 'react'
+import InnerNav from '../Shared/InnerNav'
+import AdminsNavBar from '../Shared/AdminsNavBar'
+import logo from '../assests/logo/e-election-logo.png'
+import LogoElement from '../Shared/logoElement'
+import ProfileSetting from '../Shared/ProfileSetting'
+import WelcomeMsg from '../Shared/WelcomeMsg'
+import SearchInput from '../Shared/SearchInput'
+import Colleges from './Colleges'
+import CollegeInformations from '../Shared/CollegeInformationsContainer'
+import CollegeItems from '../Shared/CollegeItems'
+import DefaultPage from './DefualtPage'
 import ConfigPage from './ConfigPage'
-import './css/MainAdmin.css';
-export const DashboardContext = createContext();
+import './css/MainAdmin.css'
+export const DashboardContext = createContext()
 const Dashboard = () => {
-    const [colleges, setColleges] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
-    const [currentCollege, setCurrentCollege] = useState('');
-    const value = {
-        colleges,
-        setColleges,
-        error,
-        setError,
-        setLoading,
-        loading,
-        setCurrentCollege,
-        currentCollege
-    }
-    const renderCurrentElement = () => {
-        switch (currentCollege) {
-            case 'config':
-                return <ConfigPage />
-            case 'profile':
-                return <div>profile</div>
-            case '':
-                return <DefaultPage />
-            default:
-                return <CollegeItems />
-        }
-    }
-    return (
-        <DashboardContext.Provider value={value}>
-            <div className='MainAdminDashboard'>
-                <AdminsNavBar>
-                    <InnerNav style={{ width: '6.5rem', backgroundColor: '#1b1e27', zIndex: 2 }} >
-                        <LogoElement src={logo} alt="app-logo" width={'5rem'} height={'5rem'}
-                            onfunction={() => setCurrentCollege('')} />
-                        <ProfileSetting />
-                    </InnerNav>
-                    <InnerNav style={{ width: '18.5rem', backgroundColor: '#222632' }}>
-                        <WelcomeMsg name={'ADMIN'} />
-                        <SearchInput />
-                        <Colleges />
-                    </InnerNav>
-                </AdminsNavBar>
-                <CollegeInformations>
-                    {renderCurrentElement()}
-                </CollegeInformations>
-            </div >
-        </DashboardContext.Provider >
-    );
-};
-export default Dashboard;
+	const [colleges, setColleges] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState('')
+	const [currentCollege, setCurrentCollege] = useState('')
+	const value = {
+		colleges,
+		setColleges,
+		error,
+		setError,
+		setLoading,
+		loading,
+		setCurrentCollege,
+		currentCollege,
+	}
+	const renderCurrentElement = () => {
+		switch (currentCollege) {
+			case 'config':
+				return <ConfigPage />
+			case 'profile':
+				return <div>profile</div>
+			case '':
+				return <DefaultPage />
+			default:
+				return <CollegeItems />
+		}
+	}
+	return (
+		<DashboardContext.Provider value={value}>
+			<div className='MainAdminDashboard'>
+				<AdminsNavBar>
+					<InnerNav
+						style={{ width: '6.5rem', backgroundColor: '#1b1e27', zIndex: 2 }}
+					>
+						<LogoElement
+							src={logo}
+							alt='app-logo'
+							width={'5rem'}
+							height={'5rem'}
+							onfunction={() => setCurrentCollege('')}
+						/>
+						<ProfileSetting />
+					</InnerNav>
+					<InnerNav style={{ width: '18.5rem', backgroundColor: '#222632' }}>
+						<WelcomeMsg name={'ADMIN'} />
+						<SearchInput />
+						<Colleges />
+					</InnerNav>
+				</AdminsNavBar>
+				<CollegeInformations>{renderCurrentElement()}</CollegeInformations>
+			</div>
+		</DashboardContext.Provider>
+	)
+}
+export default Dashboard
